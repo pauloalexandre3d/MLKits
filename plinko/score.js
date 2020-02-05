9dd8ca371e39492184c397d210b1a663ceb65936
Paulo Machado <pauloalexandre3d@gmail.com>
2020-02-05 23:58:10 +0000

    knn: Adjusting accuracity

--- plinko/score.js
+++ plinko/score.js
@@ -1,14 +1,27 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 100;
+  [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  _.range(1, 50).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) == testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log('For k of', k, 'Accuracy is ', accuracy);
+  });
+}
+
+function knn(data, point, k) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,10 +31,17 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log('Your point will probably fall into ', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
